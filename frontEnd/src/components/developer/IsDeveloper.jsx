d54d1c1e403f96ac2a2f20ae52cc187422ea7f63
dtechnicaldk <dtechnicaldk@gmail.com>
2025-01-05 13:27:16 +0530

    typing text now able to change it's statement

--- frontEnd/src/components/developer/IsDeveloper.jsx
+++ frontEnd/src/components/developer/IsDeveloper.jsx
@@ -2,31 +2,24 @@ import React, { useState } from "react";
 import "./IsDeveloper.css";
 
 function IsDeveloper() {
-  const text = [
-    "MERN stack Developer",
-    "FrontEnd Developer",
-    "BackEnd Developer",
-    "UI/UX Develper",
-  ];
-  const [index, setIndex] = useState(0);
-  //   console.log(index);
-
-  const [devType, setDevType] = useState(text[index]);
+  const [devType, setDevType] = useState("MERN stack Developer");
   let [i, setI] = useState(0);
   let [updatedeText, setUpdatedeText] = useState("");
+  let [index, setIndex] = useState(0);
 
   const typingEffect = setTimeout(() => {
     let sliceText = devType.slice(0, i);
     if (i === devType.length) {
-      setI(0);
-      //   setIndex(2);
+      const text = [
+        "FrontEnd Developer",
+        "BackEnd Developer",
+        "UI/UX Develper",
+        "MERN stack Developer",
+      ];
 
-      //   if (index === 3) {
-      //     setIndex(0);
-      //     console.log(index);
-      //   } else {
-      //     setIndex(index + 1);
-      //   }
+      setI(0);
+      index === text.length - 1 ? setIndex(0) : setIndex(index + 1);
+      setDevType(text[index]);
     } else {
       setI(i + 1);
     }
@@ -35,8 +28,8 @@ function IsDeveloper() {
 
   return (
     <div className="IsDeveloper-container">
-      <h1>Dharmendra Chauhan</h1>
-      <h3>
+      <h1 id="name1">Dharmendra Chauhan</h1>
+      <h3 id="hi-text">
         <i>
           Hi, I am <span id="text">{updatedeText}_</span>
         </i>
